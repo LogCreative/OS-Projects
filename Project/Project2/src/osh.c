d25b9183ee4c00006d03f2114372d4900321061f
LogCreative <longlaoda123456@qq.com>
2021-03-14 16:46:02 +0800

    osh history fix stdout

--- Project/Project2/src/osh.c
+++ Project/Project2/src/osh.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 #define MAX_LINE 80
 
@@ -18,9 +19,25 @@ int main(void){
         char rline[MAX_LINE];
         fgets(rline, MAX_LINE, stdin);
 
+        int flag = 0;
+
+        if (strcmp(rline,"!!\n")==0){
+            if(!buf[0]){
+                fprintf(stdout, "%s\n", "No commands in history.");
+                flag = -1;
+            } else {
+                memcpy(rline, buf, MAX_LINE*sizeof(char));
+                fprintf(stdout, "%s", rline);
+            }
+        } else memcpy(buf, rline, MAX_LINE*sizeof(char));
+
+        int fd = -1;
+        int file_mode = -1;
+        int file_arg = -1;
+
         int argc = 0;
         char prev = ' ';
-        int flag = 0;
+        
         for(char* rptr = rline; rptr < rline + MAX_LINE && !flag; ++rptr){
             switch (*rptr) {
                 case '&':
@@ -31,21 +48,13 @@ int main(void){
                     prev = *rptr;
                     *rptr = '\0';
                     break;
-                case '!':
-                    if (prev=='!' && argc == 0){
-                        if(!buf[0]){
-                            fprintf(stdout, "%s\n", "No commands in history.");
-                            flag = -1;
-                        } else {
-                            memcpy(rline, buf, MAX_LINE*sizeof(char));
-                            fprintf(stdout, "%s\n", rline);
-                            flag = 2;
-                        }
-                    }
-                    prev = *rptr;
-                    break;
-                case '\0':
-                    flag = -2;
+                case '>':
+                    file_mode = O_WRONLY;
+                case '<':
+                    file_mode = O_RDONLY;
+                    args[argc++] = NULL;
+                    prev = ' ';
+                    file_arg = argc;
                     break;
                 default:
                     if (prev == ' ') args[argc++] = rptr;
@@ -54,21 +63,41 @@ int main(void){
             }
         }
 
-        switch(flag){
-            case 1:
-                args[argc] = NULL;
-            case 2:
-                if (!args[0]) break;
-                memcpy(buf, rline, MAX_LINE*sizeof(char));
-                should_run = strcmp(args[0],"exit");
+        if(flag == -1) continue;
 
-                pid_t pid;
-                pid = fork();
+        args[argc] = NULL;
+        if (!args[0]) continue;
+        should_run = strcmp(args[0],"exit");
+        
+        int fout = dup(STDOUT_FILENO);
+        int fin = dup(STDIN_FILENO);
+        if(file_mode > 0){
+            fd = open(args[file_arg], file_mode, 0);
+            if(file_mode == O_WRONLY){
+                if(fd == -1) fd = creat(args[file_arg], 0);
+                if(fd == -1) fprintf(stderr, "%s %s.\n", "Cannot write file", args[file_arg]);
+                else dup2(fd, STDOUT_FILENO);
+            } else if (file_mode == O_RDONLY){
+                if(fd == -1) fprintf(stderr, "%s %s.\n", "Cannot read file",args[file_arg]);
+                else dup2(fd, STDIN_FILENO);
+            }
+        }
 
-                if (pid < 0) fprintf(stderr, "Fork Failed"); 
-                else if (pid == 0) return execvp(args[0],args);
-                else if (should_wait) wait(NULL);
-                break;
+        pid_t pid;
+        pid = fork();
+
+        if (pid < 0) fprintf(stderr, "Fork Failed"); 
+        else if (pid == 0) {
+            execvp(args[0],args);
+            if(fd != -1) close(fd);
+            return 0;
+        }
+        else {
+            if(file_mode > 0){
+                dup2(fin, STDIN_FILENO);
+                dup2(fout, STDOUT_FILENO);
+            }
+            if (should_wait) wait(NULL);
         }
 
     }
