8db150392abbf0ba8b2996af3764f0ac09781e67
LogCreative <longlaoda123456@qq.com>
2021-04-01 19:00:04 +0800

    P6 with a checker

--- Project/Project6/src/banker.c
+++ Project/Project6/src/banker.c
@@ -19,13 +19,106 @@ int allocation[NUMBEROFCUSTOMERS][NUMBEROFRESOURCES];
 int need[NUMBEROFCUSTOMERS][NUMBEROFRESOURCES];
 
 int request_resources(int customer_num, int request[]){
-    // 0 if successful
-    // 1 if unsuccessful
+    for(int j = 0; j < NUMBEROFRESOURCES; ++j){
+        if(allocation[customer_num][j] + request[j] > maximum[customer_num][j]){
+            fprintf(stderr, "RQ Err: Request limit exceeded.\n");
+            return 1;
+        }
+        if(request[j]>available[j]){
+            fprintf(stderr, "RQ Err: No available resources.\n");
+            return 1;
+        }
+    }
+
+    // Fake allocation
+    int available_pre[NUMBEROFRESOURCES];
+    for(int j = 0; j < NUMBEROFRESOURCES; ++j){
+        allocation[customer_num][j] += request[j];
+        need[customer_num][j] -= request[j];
+        available_pre[j] = available[j] - request[j];
+    }
+    
+    int finish[NUMBEROFCUSTOMERS];
+    for(int i = 0; i < NUMBEROFCUSTOMERS; ++i){
+        int finish_flag = 1;
+        for(int j = 0; j < NUMBEROFRESOURCES; ++j)
+            if(need[i][j] > 0)
+                finish_flag = 0;
+        finish[i] = finish_flag;
+    }
+
+    int found = 0;
+    do {
+        found = 0;
+        fprintf(stdout, "Current Available: ");
+        for(int j = 0; j < NUMBEROFRESOURCES; ++j)
+            fprintf(stdout, "%d ", available_pre[j]);
+        
+        for(int i = 0; i < NUMBEROFCUSTOMERS; ++i){
+            if(!finish[i]){
+                int next = 1;
+                for(int j = 0; j < NUMBEROFRESOURCES; ++j)
+                    if(need[i][j] > available_pre[j]){
+                        next = 0;
+                        break;
+                    }
+                if(!next) continue;
+                finish[i] = 1;
+                for(int j = 0; j < NUMBEROFRESOURCES; ++j)
+                    available_pre[j] += allocation[i][j];
+                found = 1;
+                fprintf(stdout, " > %d", i);
+                break;
+            }
+        }
+
+        fprintf(stdout, "\n");
+    } while (found);
+
+    int all_finish_flag = 1;
+    for(int i = 0; i < NUMBEROFCUSTOMERS; ++i)
+        if(!finish[i])
+            all_finish_flag = 0;
+    if(!all_finish_flag){
+        // fallback
+        for(int j = 0; j < NUMBEROFRESOURCES; ++j){
+            allocation[customer_num][j] -= request[j];
+            need[customer_num][j] += request[j];
+        }
+        fprintf(stderr, "RQ Err: Deadlock detected.\n");
+        return 1;
+    }
+
+    for(int j = 0; j < NUMBEROFRESOURCES; ++j)
+        available[j] -= request[j];
     return 0;
 }
 
 void release_resources(int customer_num, int release[]){
-
+    int real_release[NUMBEROFRESOURCES];
+    int warn_flag = 0;
+    for(int j = 0; j < NUMBEROFRESOURCES; ++j){
+        if(release[j] > allocation[customer_num][j]){
+            real_release[j] = allocation[customer_num][j];
+            warn_flag = 1;
+        }
+        else real_release[j] = release[j];
+        allocation[customer_num][j] -= real_release[j];
+        maximum[customer_num][j] -= real_release[j];
+        available[j] += real_release[j];
+    }
+    if(warn_flag){
+        fprintf(stderr, "RL Warn: Lack of allocation. Only RL ( ");
+        for(int j = 0; j < NUMBEROFRESOURCES; ++j)
+            fprintf(stdout, "%d ", real_release[j]);
+        fprintf(stdout, ").\n");
+    }
+    int fin_flag = 1;
+    for(int j = 0; j < NUMBEROFRESOURCES; ++j)
+        if(maximum[customer_num][j]>0)
+            fin_flag = 0;
+    if(fin_flag)
+        fprintf(stdout, "RL Info: Process %d is finished.\n", customer_num);
 }
 
 int main(int argc, char* argv[]){
@@ -58,6 +151,7 @@ int main(int argc, char* argv[]){
     fclose(in);
 
     char command[2];
+    fprintf(stdout,"> ");
     while(fscanf(stdin,"%s",command)!=EOF){
         if (strcmp(command,"RQ")==0){
             int customer_num;
@@ -66,7 +160,7 @@ int main(int argc, char* argv[]){
             for(int j = 0; j < NUMBEROFRESOURCES; ++j)
                 fscanf(stdin, "%d", &request[j]);
             if(request_resources(customer_num, request))
-                fprintf(stdout, "RQ Failed.\n");
+                fprintf(stdout, "RQ Denied.\n");
             else fprintf(stdout, "RQ Success.\n");
         } else if (strcmp(command,"RL")==0){
             int customer_num;
@@ -95,6 +189,7 @@ int main(int argc, char* argv[]){
                 fprintf(stdout, "\n");
             }
         }
+        fprintf(stdout,"> ");
     }
 
     return 0;
