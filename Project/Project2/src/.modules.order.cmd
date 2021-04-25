258d075f5f98d3ffbd0b441c73f8d40b395a20fb
LogCreative <longlaoda123456@qq.com>
2021-04-25 15:27:48 +0800

    clean cmd

--- Project/Project2/src/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/hgfs/VMShared/linux/OS/Project/Project2/src/modules.order := {   echo /mnt/hgfs/VMShared/linux/OS/Project/Project2/src/pid.ko; :; } | awk '!x[$$0]++' - > /mnt/hgfs/VMShared/linux/OS/Project/Project2/src/modules.order
--- Project/Project2/src/.pid.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/mnt/hgfs/VMShared/linux/OS/Project/Project2/src/pid.mod := { echo  /mnt/hgfs/VMShared/linux/OS/Project/Project2/src/pid.o;  echo; } > /mnt/hgfs/VMShared/linux/OS/Project/Project2/src/pid.mod
